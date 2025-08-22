fdb735bfb9af26d595ce6f3067653f5ef3bb9d77
Teemu Jormanainen <jormanainen.teemu@gmail.com>
2025-08-22 10:33:06 +0300

    Basic coding

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tervetuloa joukkueen luontiin");
+
+        List<Pelaaja> joukkue = new List<Pelaaja>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Anna pelaajan nimi");
+            string? nimi = Console.ReadLine();
+            if (!string.IsNullOrEmpty(nimi))
+            {
+                Pelaaja pelaaja = new Pelaaja(nimi);
+                joukkue.Add(pelaaja);
+
+            }
+        }
+        Console.WriteLine("Joukkueesi:");
+
+        foreach (Pelaaja pelaaja in joukkue)
+        {
+            Console.WriteLine(pelaaja.Nimi);
+        }
     }
 }
